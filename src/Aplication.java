59cc5e33d3cd585090394655cfbd16db44b4f9a3
VICTOR FERNANDO CARRASCO MENA <victor.carrasco@epn.edu.ec>
2019-02-16 12:23:37 -0500

    some changes
    tokenizer and manage input

--- src/Aplication.java
+++ src/Aplication.java
@@ -1,9 +1,30 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.StringTokenizer;
 
 public class Aplication {
     public static void main(String[] args) {
         try {
-            FileReader fr = new FileReader();
+            FileReader fr = new FileReader("C:\\Users\\Primario\\Documents\\GitHub\\ACME\\Roster\\roster.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String string = new String();
+            String name;
+            String schedule;
+            string = br.readLine();
+            // tokenizer for each line of the text
+            StringTokenizer tok = new StringTokenizer(string,"=");
+            name = tok.nextToken();
+            schedule = tok.nextToken();
+            // tokenizer for each day of the employee's schedule
+            StringTokenizer tok_schedule = new StringTokenizer(schedule,",");
+            System.out.println(""+name);
+            System.out.println(""+schedule);
+            String day;
+            /*while((string=br.readLine())!=null){
+                System.out.println(""+string);
+            }*/
+
+
         }catch (Exception e){
 
         }
--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,6 @@
 public class Employee {
 
+    private String name="";
+    private String days="";
+    private int hours=0;
 }
