c4a8eb50e17f2c4ceeb8aa5593da1af1cb2ac24d
VICTOR FERNANDO CARRASCO MENA <victor.carrasco@epn.edu.ec>
2019-02-16 11:09:36 -0500

    Initial commit

---
+++ src/Aplication.java
@@ -0,0 +1,11 @@
+import java.io.FileReader;
+
+public class Aplication {
+    public static void main(String[] args) {
+        try {
+            FileReader fr = new FileReader();
+        }catch (Exception e){
+
+        }
+    }
+}
